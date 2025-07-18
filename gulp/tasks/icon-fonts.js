c05cf3939bd9d4b925451bfdec8bf7a55ca744fb
Sorokovskys <Sorokovskys@ukr.net>
2025-07-18 11:04:51 +0300

    fix bug

--- gulp/tasks/icon-fonts.js
+++ gulp/tasks/icon-fonts.js
@@ -1,5 +1,5 @@
 import { path, gulp } from "../config/index.js";
 export const iconFonts = () => {
-    return gulp.src(path.src.iconFonts)
+    return gulp.src(path.src.iconFonts, {encoding: false})
     .pipe(gulp.dest(path.build.iconFonts))
 };
